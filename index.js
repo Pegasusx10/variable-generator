eb9638521442dee03f8ed42844c1db772143397b
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2022-09-27 18:58:33 +0530

    Corrected: Changed camel case using inbuilt functions
    Changed the camel case using inbuilt functions and added preview & print function for the selected string generator.

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-//  Print the text
+//  Print the
+var option='f';
+var x;
 function pr() {
-    document.getElementById("result").innerHTML = document.getElementById("result").innerHTML+"<li>"+document.getElementById('name').value+"</li>"; 
+    switch(option){
+      case 'k':
+      case 's':
+      case 'c':
+        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML+"<li>"+x+"</li>"; 
+        break;
+      default:
+        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML+"<li>"+document.getElementById('name').value+"</li>"; 
+    
+      }
+      option='f';
   }
 
 // Kebab case generator
 function kebabcase() {
+  option='k';
 var sentence = document.getElementById('name').value;
-// var words = sentence.split("A-Z")
-var x = sentence.split(" ").join("-");
+x = sentence.split(" ").join("-");
 document.getElementById("preview").innerHTML = x;
 }
 
 // Snake case generator
 function snakecase() {
+  option='s';
 var sentence = document.getElementById('name').value;
-// var words = sentence.split("A-Z")
-var x = sentence.split(" ").join("_");
+x = sentence.split(" ").join("_");
 document.getElementById("preview").innerHTML = x;
 }
 
 // camel case generator
-function cAMELCASE(){}
-
-camelize = function camelize(str) {
-    return str.replace(/\W+(.)/g, function(match, chr)
-     {
-          return chr.toUpperCase();
-      });
+function camelcase() {
+  option='c';
+  var sentence = document.getElementById('name').value;
+  var x = sentence.split(" ");
+  var camel=x['0'].toLocaleLowerCase();
+  for (let index = 1; index < x.length; index++) {
+      var temp=x[index];
+      temp=temp.toLocaleLowerCase();
+      var temp2=temp.charAt(0);
+      temp2=temp2.toUpperCase();
+      temp2=temp2+temp.slice(1);
+      camel=camel+temp2;
+  }
+  document.getElementById("preview").innerHTML = camel;
 }
