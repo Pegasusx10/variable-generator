751e46f4b89bd4c2d369fc340f3344805361648b
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2022-09-28 12:04:58 +0530

    Update: Assigned different variable name

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-//  Print the
-var option='f';
-var x;
-function pr() {
+//  Print the input string
+let option = 'submit';
+let text;
+function submitbutton() {
     switch(option){
-      case 'k':
-      case 's':
-      case 'c':
-        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML+"<li>"+x+"</li>"; 
+      case 'Kebab':
+      case 'Snake':
+      case 'Camel':
+        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML + "<li>" + text + "</li>"; 
         break;
       default:
-        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML+"<li>"+document.getElementById('name').value+"</li>"; 
+        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML + "<li>" + document.getElementById('name').value + "</li>"; 
     
       }
-      option='f';
+      option ='submit';
   }
 
 // Kebab case generator
 function kebabcase() {
-  option='k';
-var sentence = document.getElementById('name').value;
-x = sentence.split(" ").join("-");
-document.getElementById("preview").innerHTML = x;
+  option = 'Kebab';
+let sentence = document.getElementById('name').value;
+text = sentence.split(" ").join("-");
+document.getElementById("preview").innerHTML = text;
 }
 
 // Snake case generator
 function snakecase() {
-  option='s';
-var sentence = document.getElementById('name').value;
-x = sentence.split(" ").join("_");
-document.getElementById("preview").innerHTML = x;
+  option='Snake';
+let sentence = document.getElementById('name').value;
+text = sentence.split(" ").join("_");
+document.getElementById("preview").innerHTML = text;
 }
 
 // camel case generator
 function camelcase() {
-  option='c';
-  var sentence = document.getElementById('name').value;
-  var x = sentence.split(" ");
-  var camel=x['0'].toLocaleLowerCase();
-  for (let index = 1; index < x.length; index++) {
-      var temp=x[index];
+  option='Camel';
+  let sentence = document.getElementById('name').value;
+  let text = sentence.split(" ");
+  let camel=text['0'].toLocaleLowerCase();
+  for (let index = 1; index < text.length; index++) {
+      let temp=text[index];
       temp=temp.toLocaleLowerCase();
-      var temp2=temp.charAt(0);
+      let temp2=temp.charAt(0);
       temp2=temp2.toUpperCase();
       temp2=temp2+temp.slice(1);
       camel=camel+temp2;
